f69a7187fe67cde6e496bf069152caadf92a3d49
Alexandra <35624620+alexandrazd@users.noreply.github.com>
2022-03-17 15:35:06 +0200

    Add files via upload

---
+++ Homework20220310AD.R
@@ -0,0 +1,16 @@
+i = c(1:10)
+i[4]
+a <- i * 3
+b <- i[i[(length(i)-2):length(i)]]
+for(i in 1:10){
+  print(i)
+}
+ex#1
+for(i in 1:10){
+  print(i*3)
+}
+ex#2
+c = rnorm(10, mean = 0, sd= 1)
+print(c>1)
+
+ex#3
